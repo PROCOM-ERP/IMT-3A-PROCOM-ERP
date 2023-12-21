9242d7f31e2e1c940815b1bab763effd4ef0ce16
Gaël Cudennec <gael.cudennec@gmail.com>
2023-12-21 20:57:07 +0100

    feat: Http Gateway functional

---
+++ src/backend/gatewayService/src/test/java/com/example/gatewayService/GatewayServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.gatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+  // @Test
+  // void contextLoads() {
+  // }
+}
