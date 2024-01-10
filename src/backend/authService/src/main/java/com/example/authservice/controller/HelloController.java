cea7e5146adc75a6ce1c0675066279a1996db1d0
Gaël Cudennec <gael.cudennec@gmail.com>
2024-01-10 13:53:29 +0100

    fix: small stash

--- src/backend/authService/src/main/java/com/example/authservice/controller/HelloController.java
+++ src/backend/authService/src/main/java/com/example/authservice/controller/HelloController.java
@@ -19,6 +19,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+// @RestController
+// @RequestMapping("/api/v1/hello")
+// public class HelloController {
+//
+//     @GetMapping(produces = "application/json")
+//     @Operation(
+//             summary = "GET Hello World !",
+//             description = "GET Hello World ! from AuthService",
+//             operationId = "getHello",
+//             tags = {"hello"}
+//     )
+//     public @ResponseBody ResponseEntity<String> getHello() {
+//         return ResponseEntity
+//                 .status(HttpStatus.OK)
+//                 .contentType(MediaType.APPLICATION_JSON)
+//                 .body("Hello, World !");
+//     }
+//
+// }
 @RestController
 @RequestMapping(Path.V1_HELLO)
 @RequiredArgsConstructor
