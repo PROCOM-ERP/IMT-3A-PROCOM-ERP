33111066f562ee2c45df5256d7ffd4cc7667f124
yunea <code.experience01@gmail.com>
2024-01-24 20:29:55 +0100

    add directory, inventory, order management pages

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Authentification from './pages/Authentification.js';
 import Home from './pages/Home.js';
 import Profil from './pages/Profil.js';
+import Directory from './pages/Directory.js';
+import Inventory from './pages/Inventory.js';
+import OrderManagement from './pages/OrderManagement.js';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Routes>
           <Route path='/' exact Component={Authentification} />
           <Route path='/home' exact Component={Home} />
+          <Route path='/directory' exact Component={Directory} />
+          <Route path='/inventory' exact Component={Inventory} />
+          <Route path='/orderManagement' exact Component={OrderManagement} />
           <Route path='/profil' exact Component={Profil} />
         </Routes>
       </Router>
---
+++ src/frontend/src/pages/Directory.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function Directory() {
+  return (
+    <>
+    <Navbar/>
+    <h1>Directory</h1>
+    </>
+  )
+}
+
+export default Directory
