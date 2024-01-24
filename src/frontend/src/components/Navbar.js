d150e87c6b3d9c99b9a12d08b9027e774e82e29e
yunea <code.experience01@gmail.com>
2024-01-24 20:13:27 +0100

    add onglet in navbar

--- src/frontend/src/components/Navbar.js
+++ src/frontend/src/components/Navbar.js
@@ -3,16 +3,24 @@ import React from 'react'
 
 
 function Navbar() {
+    const companyName = "ERP FIP";
   return (
     <>
     <nav className='navigation'>
-        <a href="/" className="brand-name">
-            ERP FIP
-        </a>
+        <a href="/" className="brand-name">{companyName}</a>
         <div className="navigation-menu">
             <ul>
                 <li>
                     <a href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/directory">Directory</a>
+                </li>
+                <li>
+                    <a href="/inventory">Inventory</a>
+                </li>
+                <li>
+                    <a href="/orderManagement">Order Management</a>
                 </li>
                 <li>
                     <a href="/profil">Profil</a>
