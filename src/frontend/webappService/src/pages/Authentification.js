acf419428609dea0482b32a46f86a36350cff9eb
yunea <code.experience01@gmail.com>
2024-01-26 22:32:51 +0100

    rename file Form.js to AuthForm.js

--- src/frontend/webappService/src/pages/Authentification.js
+++ src/frontend/webappService/src/pages/Authentification.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Form from '../components/Form.js';
+import AuthForm from '../components/AuthForm.js';
 import AuthHelloWorld from '../components/AuthHelloWorld.js';
 
 function Authentification() {
   return (
     <div>
       <h1>Login</h1>
-      <Form/>
+      <AuthForm/>
     </div>
   )
 }
