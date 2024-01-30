793df7f2ea2dc11bd8eb48a46528583ae82ba267
Gaël Cudennec <gael.cudennec@gmail.com>
2024-01-30 23:30:49 +0100

    system: first basic trembling API request

--- src/frontend/webappService/src/pages/Profil.js
+++ src/frontend/webappService/src/pages/Profil.js
@@ -1,29 +1,56 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
 
 function Profil() {
-    const user = {
-        id : 1,
-        firstname : 'Aina',
-        lastname : 'Dirou',
-        age : 23,
-        job : 'Frontend Developper', 
-        office : 'Brest',
-        department : 'Software Engineering'
-    }
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Define the API URL
+    const apiUrl = "https://localhost:8041/api/dir/v1/hello";
+
+    // Make the API request
+    fetch(apiUrl, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.text(); // Parse the response as text
+      })
+      .then((data) => {
+        console.log("API Response:", data); // Check the response in the console
+        setMessage(data); // Set the response as the message
+      })
+      .catch((error) => {
+        console.error("API request error:", error);
+      });
+  }, []);
+
+  const user = {
+    id: 1,
+    firstname: "Aina",
+    lastname: "Dirou",
+    age: 23,
+    job: "Frontend Developper",
+    office: "Brest",
+    department: "Software Engineering",
+  };
 
   return (
     <div>
-        <Navbar/>
-        <h1>Profil</h1>
-        <p>Firstname : {user.firstname}</p>
-        <p>Lastname : {user.lastname}</p>
-        <p>Age : {user.age}</p>
-        <p>Office : {user.office}</p>
-        <p>Department : {user.department}</p>
-        <p>Job : {user.job}</p>
+      <Navbar />
+      <h1>Profil</h1>
+      <p>Firstname : {user.firstname}</p>
+      <p>Lastname : {user.lastname}</p>
+      <p>Age : {user.age}</p>
+      <p>Office : {user.office}</p>
+      <p>Department : {user.department}</p>
+      <p>Job : {user.job}</p>
+      <p>API Response: {message}</p>
     </div>
-  )
+  );
 }
 
-export default Profil
+export default Profil;
