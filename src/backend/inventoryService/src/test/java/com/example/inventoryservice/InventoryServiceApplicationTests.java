f6c86b47dfef5ba3031782c21415f527ca121e89
Arthur Maquin <arthurmaquin.2001@gmail.com>
2024-02-08 10:18:42 +0100

    comment InventoryServiceApplicationTests class

--- src/backend/inventoryService/src/test/java/com/example/inventoryservice/InventoryServiceApplicationTests.java
+++ src/backend/inventoryService/src/test/java/com/example/inventoryservice/InventoryServiceApplicationTests.java
@@ -5,9 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InventoryServiceApplicationTests {
-
+/*
     @Test
     void contextLoads() {
-    }
+    }*/
 
 }
