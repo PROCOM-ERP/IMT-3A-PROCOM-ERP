f37308d30220be92a9065e315619cb1868d31d6d
Arthur Maquin <arthurmaquin.2001@gmail.com>
2024-02-08 11:51:02 +0100

    minor commit

--- src/backend/inventoryService/src/main/java/com/example/inventoryservice/controller/AddressController.java
+++ src/backend/inventoryService/src/main/java/com/example/inventoryservice/controller/AddressController.java
@@ -9,8 +9,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
