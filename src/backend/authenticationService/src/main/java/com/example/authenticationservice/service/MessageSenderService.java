504083b6a84477ab28f2202a3a7ed26e0afd08d0
bops <thibaut.ruzicka@imt-atlantique.net>
2024-02-11 22:11:57 +0100

    MessageSenderService rolesFanoutExchange methods added

---
+++ src/backend/authenticationService/src/main/java/com/example/authenticationservice/service/MessageSenderService.java
@@ -0,0 +1,60 @@
+package com.example.authenticationservice.service;
+
+import com.example.authenticationservice.model.Path;
+import com.example.authenticationservice.utils.CustomHttpRequestBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageSenderService {
+
+    private final CustomHttpRequestBuilder customHttpRequestBuilder;
+    private final RabbitTemplate rabbitTemplate;
+    @Qualifier("rolesFanoutExchange")
+    private final FanoutExchange rolesFanoutExchange;
+    @Qualifier("loginProfilesSecExchange")
+    private final FanoutExchange loginProfilesSecExchange;
+
+    private final Logger logger = LoggerFactory.getLogger(MessageSenderService.class);
+
+    @Autowired
+    public MessageSenderService(CustomHttpRequestBuilder customHttpRequestBuilder, RabbitTemplate rabbitTemplate, FanoutExchange rolesFanoutExchange, FanoutExchange loginProfilesSecExchange) {
+        this.customHttpRequestBuilder = customHttpRequestBuilder;
+        this.rabbitTemplate = rabbitTemplate;
+        this.rolesFanoutExchange = rolesFanoutExchange;
+        this.loginProfilesSecExchange = loginProfilesSecExchange;
+    }
+
+    public void sendRolesNewMessage(String roleName) {
+        logger.info("Sending message to inform the network about role creation...");
+        String resource = String.format("%s/%s%s", Path.ROLES, roleName, Path.ACTIVATION);
+        String path = customHttpRequestBuilder.buildPath(Path.V1, resource);
+        rabbitTemplate.convertAndSend(rolesFanoutExchange.getName(), "role.activation", path);
+        logger.info("Message sent");
+    }
+
+    public void sendLoginProfilesJwtDisableOldMessage() {
+        logger.info("Sending message to expire all login profile Jwt...");
+        //rabbitTemplate.convertAndSend(loginProfilesSecExchange.getName(), "login-profiles.jwt.disable.old", "");
+        logger.info("Message sent");
+    }
+
+    public void sendLoginProfileJwtDisableOldMessage(String idLoginProfile) {
+        logger.info("Sending message to expire a login profile Jwt...");
+        //rabbitTemplate.convertAndSend(loginProfilesSecExchange.getName(), "login-profile.jwt.disable.old", idLoginProfile);
+        logger.info("Message sent");
+    }
+
+    public void sendLoginProfileEnableModify(String idLoginProfile) {
+        logger.info("Sending message to set a login profile activation status...");
+        //String resource = String.format("%s/%s%s", Path.LOGIN_PROFILES, idLoginProfile, Path.ENABLE);
+        //String path = customHttpRequestBuilder.buildPath(Path.V1, resource);
+        //rabbitTemplate.convertAndSend(loginProfilesSecExchange.getName(), "login-profile.enable.modify", path);
+        logger.info("Message sent");
+    }
+}
