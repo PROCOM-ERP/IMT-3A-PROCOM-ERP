ff349036b4ccf65226f74f4780d89858ce98751c
bops <thibaut.ruzicka@imt-atlantique.net>
2024-02-11 21:50:20 +0100

    MessageReceiverService methods added

--- src/backend/authenticationService/src/main/java/com/example/authenticationservice/service/MessageReceiverService.java
+++ src/backend/authenticationService/src/main/java/com/example/authenticationservice/service/MessageReceiverService.java
@@ -1,6 +1,5 @@
 package com.example.authenticationservice.service;
 
-import com.example.authenticationservice.utils.CustomHttpRequestBuilder;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +12,6 @@ import org.springframework.web.client.RestClientException;
 public class MessageReceiverService {
 
     private final RoleService roleService;
-    private final CustomHttpRequestBuilder customHttpRequestBuilder;
 
     private final Logger logger = LoggerFactory.getLogger(MessageReceiverService.class);
 
@@ -21,6 +19,7 @@ public class MessageReceiverService {
     public void receiveRolesInitMessage(String getAllRolesPath) {
         logger.info("Message received on startup of a service to init its roles: " + getAllRolesPath);
         try {
+            // try to save all microservice roles
             roleService.saveAllMicroserviceRoles(getAllRolesPath);
             logger.info("Roles successfully initialised");
         } catch (RestClientException ignored) {
@@ -31,20 +30,12 @@ public class MessageReceiverService {
     @RabbitListener(queues = "role-activation-queue")
     public void receiveRoleEnableModifyMessage(String getRoleByNamePath) {
         logger.info("Message received to set a role activation status: " + getRoleByNamePath);
-        /*
         try {
-            // try to get external role
-            RoleActivationDto roleActivation = roleService.getExternalRole(getRoleByNamePath);
-            // update local roles
-            roleService.saveExternalRole(roleActivation);
+            // try to save microservice role
+            roleService.saveMicroserviceRole(getRoleByNamePath);
             logger.info("Role activation status successfully set");
-        } catch (NoSuchElementException | RestClientException ignored) {
+        } catch (RestClientException ignored) {
             logger.error("Role activation status set failed");
         }
-
-         */
     }
-
-
-
 }
