c8ccb95cd8e6e41ca26e8cc5a3cb0a8e2eedf307
yunea <code.experience01@gmail.com>
2024-02-12 18:01:13 +0100

    display permissions added (admin) in progress

--- src/frontend/webappService/src/components/DisplayPermissions.js
+++ src/frontend/webappService/src/components/DisplayPermissions.js
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from 'react'
 
 function DisplayPermissions() {
   const [services, setServices] = useState({ 0: "Authentification", 1: "Directory", 2: "Inventary" });
-  const [roles, setRoles] = useState({ 0: "Admin", 1: "Employee" });
+  const [roles, setRoles] = useState({ 0: "Administrator", 1: "Employee" });
   const [selectedService, setSelectedService] = useState('');
   const [selectedRole, setSelectedRole] = useState('');
+  const [isEnabled, setIsEnabled] = useState();
   const [permissions, setPermissions] = useState({});
 
-  const [isSelected, setIsSelected] = useState(false); // to set true if a service AND a role is selected
+  const [areSelected, setAreSelected] = useState(false); // to set true if a service AND a role is selected
 
   const user = localStorage.getItem('id');
   const token = localStorage.getItem('Token');
 
   const headers = { 'Authorization': `Bearer ${token}` };
-
 
   // TODO
   // const getServicesAndRoles = async () => {
@@ -39,58 +39,127 @@ function DisplayPermissions() {
   // }
 
   // TODO
-  // const getPermissions = async () => {
-  //   const apiUrl = "https://localhost:8041/api/auth/v1/auth/jwt"; // TODO: to change
-  //   // Make the API request
-  //   await fetch(apiUrl, {
-  //     method: "GET",
-  //     headers: headers,
-  //   })
-  //     .then((response) => {
-  //       if (!response.ok) throw new Error(response.status);
-  //       const res = response.json();
-  //       return res;
-  //     })
-  //     .then(data => {
-  //       setPermissions(data);
-  //       console.log("[LOG] Permissions retrieved");
-  //     })
-  //     .catch(error => {
-  //       console.error('API request error: ', error);
-  //     });
-  // }
+  const getPermissions = async () => {
+    console.log("areSelected: ", areSelected);
+    console.log("Service selected: ", selectedService);
+    console.log("Role selected: ", selectedRole);
+
+    if (areSelected === true) {
+      setPermissions({ "read": false, "write": false, "delete": false });
+      // setSelectedRole('');
+      // setSelectedService('');
+    } else if (Object.keys(permissions).length > 0) {
+      setPermissions({});
+    }
+
+    // const apiUrl = "https://localhost:8041/api/auth/v1/auth/jwt"; // TODO: to change
+    // // Make the API request
+    // await fetch(apiUrl, {
+    //   method: "GET",
+    //   headers: headers,
+    // })
+    //   .then((response) => {
+    //     if (!response.ok) throw new Error(response.status);
+    //     const res = response.json();
+    //     return res;
+    //   })
+    //   .then(data => {
+    //     setPermissions(data);
+    //     console.log("[LOG] Permissions retrieved");
+    //   })
+    //   .catch(error => {
+    //     console.error('API request error: ', error);
+    //   });
+
+  }
+
+  const handleSelection = () => {
+    if (selectedService.length > 0 && selectedRole.length > 0) {
+      setAreSelected(true);
+      setSelectedRole('');
+      setSelectedService('');
+    } else {
+      setAreSelected(false);
+    }
+  }
 
   const handleServiceChange = (e) => {
     setSelectedService(e.target.value);
+    handleSelection();
   };
 
   const handleRoleChange = (e) => {
     setSelectedRole(e.target.value);
+    handleSelection();
   };
 
-  const handleSelect = () => {
-    if (selectedService && selectedRole) {
-      setIsSelected(true);
+  const handlePermissionChange = (e) => {
+    const { name, checked } = e.target;
+    setPermissions(prevPermissions => ({
+      ...prevPermissions,
+      [name]: checked
+    }));
+  };
+
+  const displayPermissions = () => {
+    //handleSelect();
+    //console.log("display permissions: ", areSelected)
+    // if (areSelected) {
+    getPermissions();
+    console.log("Permissions: ", (Object.keys(permissions).length > 0));
+    console.log(permissions);
+
+    if (Object.keys(permissions).length > 0) {
+
+      return (
+        <>
+          <div>
+            <h2> {selectedService}-{selectedRole}: Permissions Details</h2>
+            <form>
+              {Object.entries(permissions).map(([key, value]) => (
+                <div key={key}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name={key}
+                      checked={value}
+                      onChange={handlePermissionChange}
+                    />
+                    {key}
+                  </label>
+                </div>
+              ))}
+            </form>
+          </div>
+        </>
+      )
+
+    } else {
+      return (
+        <>
+          <p>Select a service and a role to display permissions.</p>
+        </>
+      )
     }
   }
 
   useEffect(() => {
     // getServicesAndRoles();
-    handleSelect();
-    // if (isSelected) {
-    //   getPermissions();
-    // }
+    // getPermissions();
+    // console.log("areSelected: ", areSelected);
+    // displayPermissions();
+    handleSelection();
   }, []);
 
   return (
     <>
-      <h1>Select Services and Roles</h1>
+      <h1 >Permissions</h1>
       <div>
-        <label htmlFor="services">Select Service:</label>
+        <label htmlFor="services">Select Service: </label>
         <select id="services" value={selectedService} onChange={handleServiceChange}>
           <option value="">Select a service</option>
-          {Object.entries(services).map((key, value) => (
-            <option key={key} >{value}</option>
+          {Object.entries(services).map(([key, value]) => (
+            <option key={key} value={value} >{value}</option>
           ))}
         </select>
       </div>
@@ -98,14 +167,17 @@ function DisplayPermissions() {
         <label htmlFor="roles">Select Role:</label>
         <select id="roles" value={selectedRole} onChange={handleRoleChange}>
           <option value="">Select a role</option>
-          {Object.entries(roles).map((key, value) => (
-            <option key={key} > {value} </option>
+          {Object.entries(roles).map(([key, value]) => (
+            <option key={key} value={value} > {value} </option>
           ))}
         </select>
       </div>
       <div>
         <p>Selected Service: {selectedService}</p>
         <p>Selected Role: {selectedRole}</p>
+      </div>
+      <div>
+        {displayPermissions()}
       </div>
 
     </>
