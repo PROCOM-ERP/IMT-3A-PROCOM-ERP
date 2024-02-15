6d848794a3636ef04274ae5371e0354e5f898ad8
bops <thibaut.ruzicka@imt-atlantique.net>
2024-02-15 23:09:24 +0100

    Update: Directory Service Java model

---
+++ src/backend/directoryService/src/main/java/com/example/directoryservice/model/LoginProfile.java
@@ -0,0 +1,37 @@
+package com.example.directoryservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.Instant;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "login_profiles", schema = "public")
+public class LoginProfile {
+    @Id
+    @Size(max = 6)
+    @Column(name = "id", nullable = false, length = 6)
+    private String id;
+
+    @Builder.Default
+    @NotNull
+    @Column(name = "is_enable", nullable = false)
+    private Boolean isEnable = true;
+
+    @Builder.Default
+    @NotNull
+    @Column(name = "jwt_gen_min_at", nullable = false)
+    private Instant jwtGenMinAt = Instant.now();
+
+    @Builder.Default
+    @OneToOne(mappedBy = "id")
+    private Employee employee = null;
+
+}
