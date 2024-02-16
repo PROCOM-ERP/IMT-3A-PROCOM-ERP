0f0ddbac7d1fe2bfc5339561b818c9b17730abc0
bops <thibaut.ruzicka@imt-atlantique.net>
2024-02-17 00:52:55 +0100

    Fix: Directory Addresses Permissions

--- src/backend/directoryService/src/main/java/com/example/directoryservice/controller/AddressController.java
+++ src/backend/directoryService/src/main/java/com/example/directoryservice/controller/AddressController.java
@@ -13,9 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.Set;
 
 @RestController
