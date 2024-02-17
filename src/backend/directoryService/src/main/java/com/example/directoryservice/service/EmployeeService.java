05b0d28ff6f8587040fc24d77d51851380457276
bops <thibaut.ruzicka@imt-atlantique.net>
2024-02-17 01:09:39 +0100

    Fix: Directory update Employee

--- src/backend/directoryService/src/main/java/com/example/directoryservice/service/EmployeeService.java
+++ src/backend/directoryService/src/main/java/com/example/directoryservice/service/EmployeeService.java
@@ -12,7 +12,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
