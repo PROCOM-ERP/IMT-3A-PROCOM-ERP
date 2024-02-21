708b792b756f57796c5efef5dc86b072117bb038
maestro-bene <gael.cudennec@gmail.com>
2024-02-21 20:06:52 +0100

    fix: maven dependencies versions controlled in env level

--- src/frontend/webappService/src/components/DirectoryTable.js
+++ src/frontend/webappService/src/components/DirectoryTable.js
@@ -1,119 +1,119 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../css/DirectoryTable.css";
 
 function DirectoryTable() {
-    //const defaultUsers = data.users;
-    const navigate = useNavigate();
-    const userId = localStorage.getItem("id");
-    const [users, setUsers] = useState([]);
+  //const defaultUsers = data.users;
+  const navigate = useNavigate();
+  const userId = localStorage.getItem("id");
+  const [users, setUsers] = useState([]);
 
-    const [searchTerm, setSearchTerm] = useState('');
-    const [sortBy, setSortBy] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortBy, setSortBy] = useState(null);
 
-    const handleChange = (e) => {
-        setSearchTerm(e.target.value);
-    };
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
-    const handleSort = (key) => {
-        setSortBy(key);
-    };
+  const handleSort = (key) => {
+    setSortBy(key);
+  };
 
-    const handleProfil = (id) => {
-        console.log("tr click: ", id);
-        navigate("/user/" + id);
-    }
+  const handleProfil = (id) => {
+    console.log("tr click: ", id);
+    navigate("/user/" + id);
+  };
 
-    // TODO : tester si la valeur est null -> ne pas appliquer le filtre
-    let filteredUsers = users.filter(user =>
-        user.id.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        user.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        user.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        user.email.toLowerCase().includes(searchTerm.toLowerCase())
-        //user.phoneNumber.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  // TODO : tester si la valeur est null -> ne pas appliquer le filtre
+  let filteredUsers = users.filter(
+    (user) =>
+      user.id.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchTerm.toLowerCase()),
+    //user.phoneNumber.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-    if (sortBy) {
-        filteredUsers.sort((a, b) => {
-            if (a[sortBy] < b[sortBy]) return -1;
-            if (a[sortBy] > b[sortBy]) return 1;
-            return 0;
-        });
-    }
+  if (sortBy) {
+    filteredUsers.sort((a, b) => {
+      if (a[sortBy] < b[sortBy]) return -1;
+      if (a[sortBy] > b[sortBy]) return 1;
+      return 0;
+    });
+  }
 
-    const tokenName = "Token"; // Need to be the same name as in AuthForm.js components
-    const token = localStorage.getItem(tokenName);
-    const apiUrl = "https://localhost:8041/api/dir/v1/employees";
+  const tokenName = "Token"; // Need to be the same name as in AuthForm.js components
+  const token = localStorage.getItem(tokenName);
+  const apiUrl = "https://localhost:8041/api/directory/v1/employees";
 
-    const headers = {
-        'Authorization': `Bearer ${token}`,
-    };
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
 
-    const getEmployees = async () => {
+  const getEmployees = async () => {
+    // Make the API request
+    await fetch(apiUrl, {
+      method: "GET",
+      headers: headers,
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error(response.status);
+        const res = response.json();
+        return res;
+      })
+      .then((data) => {
+        setUsers(data);
+        console.log("[LOG] Users profil information retrieved");
+      })
+      .catch((error) => {
+        console.error("API request error: ", error);
+      });
+  };
 
-        // Make the API request
-        await fetch(apiUrl, {
-            method: "GET",
-            headers: headers,
-        })
-            .then((response) => {
-                if (!response.ok) throw new Error(response.status);
-                const res = response.json();
-                return res;
-            })
-            .then(data => {
-                setUsers(data);
-                console.log("[LOG] Users profil information retrieved");
-            })
-            .catch(error => {
-                console.error('API request error: ', error);
-            });
-    }
+  useEffect(() => {
+    getEmployees();
+  }, []);
 
-    useEffect(() => {
-        getEmployees();
-    }, []);
-
-    return (
-        <>
-            <div className='directory-container'>
-                <input className='searchBar'
-                    type="text"
-                    placeholder="Search"
-                    value={searchTerm}
-                    onChange={handleChange}
-                />
-                <table className='table-container' >
-                    <thead className='table-head-container'>
-                        <tr>
-                            <th onClick={() => handleSort('id')} >ID</th>
-                            <th onClick={() => handleSort('firstName')} >Firstname</th>
-                            <th onClick={() => handleSort('lastName')} >Lastname</th>
-                            <th onClick={() => handleSort('email')} >Email</th>
-                            <th onClick={() => handleSort('phoneNumber')} >Phone number</th>
-                        </tr>
-                    </thead>
-                    <tbody className='table-body-container'>
-                        {filteredUsers.map((user, index) => {
-                            return (
-                                <tr key={index} onClick={() => handleProfil(user.id)} >
-                                    <td>
-                                        {/* <Link className='table-link' to={`/user/${user.id}`}>{user.id} </Link> */}
-                                        {user.id}
-                                    </td>
-                                    <td> {user.firstName} </td>
-                                    <td> {user.lastName} </td>
-                                    <td> {user.email} </td>
-                                    <td> {user.phoneNumber} </td>
-                                </tr>
-                            )
-                        })}
-
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="directory-container">
+        <input
+          className="searchBar"
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <table className="table-container">
+          <thead className="table-head-container">
+            <tr>
+              <th onClick={() => handleSort("id")}>ID</th>
+              <th onClick={() => handleSort("firstName")}>Firstname</th>
+              <th onClick={() => handleSort("lastName")}>Lastname</th>
+              <th onClick={() => handleSort("email")}>Email</th>
+              <th onClick={() => handleSort("phoneNumber")}>Phone number</th>
+            </tr>
+          </thead>
+          <tbody className="table-body-container">
+            {filteredUsers.map((user, index) => {
+              return (
+                <tr key={index} onClick={() => handleProfil(user.id)}>
+                  <td>
+                    {/* <Link className='table-link' to={`/user/${user.id}`}>{user.id} </Link> */}
+                    {user.id}
+                  </td>
+                  <td> {user.firstName} </td>
+                  <td> {user.lastName} </td>
+                  <td> {user.email} </td>
+                  <td> {user.phoneNumber} </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
-export default DirectoryTable
+export default DirectoryTable;
