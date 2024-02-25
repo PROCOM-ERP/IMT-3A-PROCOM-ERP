b04426e51777352125bf71e8ff16061a43334416
bops <thibaut.ruzicka@imt-atlantique.net>
2024-02-25 23:01:45 +0100

    Add: OrderService Spring config folder

---
+++ src/backend/orderService/src/main/java/com/example/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,65 @@
+package com.example.orderservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+@DependsOn({"securityConfig", "restConfig"})
+public class RabbitMQConfig {
+
+    /* Queues */
+
+    @Bean
+    public Queue rolesNewQueue() {
+        return new Queue("roles-new-queue");
+    }
+
+    @Bean
+    public Queue loginProfilesSecQueue() {
+        return new Queue("login-profiles-sec-queue");
+    }
+
+    /* Exchanges */
+
+    @Bean
+    public DirectExchange rolesDirectExchange() {
+        return new DirectExchange("roles-direct-exchange");
+    }
+
+    @Bean
+    public FanoutExchange rolesFanoutExchange() {
+        return new FanoutExchange("roles-fanout-exchange");
+    }
+
+    @Bean
+    public FanoutExchange loginProfilesSecExchange() {
+        return new FanoutExchange("login-profiles-sec-exchange");
+    }
+
+    @Bean
+    public TopicExchange employeesExchange() {
+        return new TopicExchange("employees-exchange");
+    }
+
+    /* Bindings */
+
+    @Bean
+    public Binding rolesNewBinding(Queue rolesNewQueue,
+                                   Exchange rolesFanoutExchange) {
+        return BindingBuilder.bind(rolesNewQueue)
+                .to(rolesFanoutExchange)
+                .with("*")
+                .noargs();
+    }
+
+    @Bean
+    public Binding loginProfilesSecBinding(Queue loginProfilesSecQueue,
+                                           Exchange loginProfilesSecExchange) {
+        return BindingBuilder.bind(loginProfilesSecQueue)
+                .to(loginProfilesSecExchange)
+                .with("*")
+                .noargs();
+    }
+}
