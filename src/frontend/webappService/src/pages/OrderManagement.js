fc3e2816b2e10a97cea05cc8d0e0242f8135681f
yunea <code.experience01@gmail.com>
2024-02-26 14:45:03 +0100

    add Displayorders component in OrderManagement page

--- src/frontend/webappService/src/pages/OrderManagement.js
+++ src/frontend/webappService/src/pages/OrderManagement.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import DisplayOrders from '../components/DisplayOrders'
 
 function OrderManagement() {
   return (
     <>
       <Navbar />
       <div className='title'>Order Management</div>
+      <DisplayOrders />
     </>
   )
 }
