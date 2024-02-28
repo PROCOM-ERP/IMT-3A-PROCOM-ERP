c131117dd7a76ba41b1b1e7234d86346c726c271
bops <thibaut.ruzicka@imt-atlantique.net>
2024-02-28 23:01:04 +0100

    Update: E03-US03-T01 AddressCreationRequestDto format constraints

--- src/backend/directoryService/src/main/java/com/example/directoryservice/dto/AddressCreationRequestDto.java
+++ src/backend/directoryService/src/main/java/com/example/directoryservice/dto/AddressCreationRequestDto.java
@@ -1,22 +1,30 @@
 package com.example.directoryservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.lang.Nullable;
 
-@Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Getter
+@Setter
 public class AddressCreationRequestDto {
 
+    @NotNull(message = "Address number field cannot be null and has to be an integer")
     private Integer number;
+    @NotBlank(message = "Address street field cannot be null or blank")
     private String street;
+    @NotBlank(message = "Address city field cannot be null or blank")
     private String city;
+    @Nullable
     private String state;
+    @NotBlank(message = "Address country field cannot be null or blank")
     private String country;
+    @NotBlank(message = "Address zipcode field cannot be null or blank")
     private String zipcode;
+    @Nullable
     private String info;
 
 }
--- src/backend/orderService/src/main/java/com/example/orderservice/dto/AddressCreationRequestDto.java
+++ src/backend/orderService/src/main/java/com/example/orderservice/dto/AddressCreationRequestDto.java
@@ -1,22 +1,30 @@
 package com.example.orderservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.lang.Nullable;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class AddressCreationRequestDto {
 
+    @NotNull(message = "Address number field cannot be null and has to be an integer")
     private Integer number;
+    @NotBlank(message = "Address street field cannot be null or blank")
     private String street;
+    @NotBlank(message = "Address city field cannot be null or blank")
     private String city;
+    @Nullable
     private String state;
+    @NotBlank(message = "Address country field cannot be null or blank")
     private String country;
+    @NotBlank(message = "Address zipcode field cannot be null or blank")
     private String zipcode;
+    @Nullable
     private String info;
 
 }
