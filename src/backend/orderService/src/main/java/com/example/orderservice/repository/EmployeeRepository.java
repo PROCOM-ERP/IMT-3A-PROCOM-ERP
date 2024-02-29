0c490fe2d2f13a62fc6d8d6cf35927bf78b1a0ca
bops <thibaut.ruzicka@imt-atlantique.net>
2024-02-29 17:59:40 +0100

    Fix: E03-US04-T01 EmployeeRepository.java imports

--- src/backend/orderService/src/main/java/com/example/orderservice/repository/EmployeeRepository.java
+++ src/backend/orderService/src/main/java/com/example/orderservice/repository/EmployeeRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
-import java.util.List;
 import java.util.Set;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>, JpaSpecificationExecutor<Employee> {
