4672fbec0b669314638cfb36cf3e94a8ba4a5d52
maestro-bene <gael.cudennec@gmail.com>
2024-03-03 14:36:34 +0100

    refactor(Exp Backoff): Message utils created

---
+++ src/backend/orderService/src/main/java/com/example/orderservice/utils/MessageUtils.java
@@ -0,0 +1,19 @@
+package com.example.orderservice.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageUtils {
+
+    private static final long BASE_DELAY = 1000;
+
+    /**
+     * Calculates the delay for exponential backoff.
+     * 
+     * @param retryCount the current retry attempt count.
+     * @return the delay in milliseconds before the next retry attempt.
+     */
+    public long calculateExponentialBackoff(int retryCount) {
+        return (long) (Math.pow(2, retryCount) * BASE_DELAY);
+    }
+}
