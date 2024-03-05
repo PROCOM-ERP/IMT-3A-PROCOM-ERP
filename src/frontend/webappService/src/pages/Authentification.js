b3a584941925db921c7dbf7cc1188d82e520b9dc
yunea <code.experience01@gmail.com>
2024-03-05 21:13:09 +0100

    add error pages (401 and 403)

--- src/frontend/webappService/src/pages/Authentification.js
+++ src/frontend/webappService/src/pages/Authentification.js
@@ -1,15 +1,11 @@
 import '../css/App.css';
 import React from 'react';
 import AuthForm from '../components/AuthForm.js';
-import AuthHelloWorld from '../components/AuthHelloWorld.js';
-import DirHelloWorld from '../components/DirHelloWorld.js';
 
 function Authentification() {
   return (
     <>
-    <AuthHelloWorld/>
-    <DirHelloWorld/>
-    <AuthForm/>
+      <AuthForm />
     </>
   )
 }
--- src/frontend/webappService/src/pages/Error404.js
+++ src/frontend/webappService/src/pages/Error404.js
@@ -6,8 +6,8 @@ function Error404() {
   return (
     <>
       <Navbar />
-      <div className='title'>Error 404: page not found</div>
-      <div className='text'> <Link to="/home" >Go back to home page</Link> </div>
+      <div className='title'>Error 404: Not found</div>
+      <div className='text'> <Link to="/home" >Go back to home</Link> </div>
     </>
   )
 }
