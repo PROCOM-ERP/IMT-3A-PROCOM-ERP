044581f5b336070c753119d515174d27f783b0b2
yunea <code.experience01@gmail.com>
2024-03-06 20:12:45 +0100

    add redirection to error pages in UserProfil

--- src/frontend/webappService/src/components/UserProfil.js
+++ src/frontend/webappService/src/components/UserProfil.js
@@ -34,7 +34,11 @@ function UserProfil({ title, userId }) {
       headers: headers,
     })
       .then((response) => {
-        if (!response.ok) throw new Error(response.status);
+        if (!response.ok) {
+          if (response.status === 401) { navigate("/error401"); }
+          else if (response.status === 403) { navigate("/error403"); }
+          else { throw new Error(response.status + " " + response.statusText); }
+        }
         const res = response.json();
         return res;
       })
