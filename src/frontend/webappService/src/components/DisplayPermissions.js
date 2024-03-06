2186788909e1c4e37fcc2f96fe7a32f900ef5dba
yunea <code.experience01@gmail.com>
2024-03-06 13:53:41 +0100

    add errpr redirection to DisplayPermissions (Permissions page)

--- src/frontend/webappService/src/components/DisplayPermissions.js
+++ src/frontend/webappService/src/components/DisplayPermissions.js
@@ -51,7 +51,11 @@ function DisplayPermissions() {
       headers: headers,
     })
       .then((response) => {
-        if (!response.ok) throw new Error(response.status);
+        if (!response.ok) {
+          if (response.status === 401) { navigate("/error401"); }
+          else if (response.status === 403) { navigate("/error403"); }
+          else { throw new Error(response.status + " " + response.statusText); }
+        }
         const res = response.json();
         return res;
       })
@@ -101,7 +105,11 @@ function DisplayPermissions() {
       headers: headers,
     })
       .then((response) => {
-        if (!response.ok) throw new Error(response.status);
+        if (!response.ok) {
+          if (response.status === 401) { navigate("/error401"); }
+          else if (response.status === 403) { navigate("/error403"); }
+          else { throw new Error(response.status + " " + response.statusText); }
+        }
         const res = response.json();
         return res;
       })
