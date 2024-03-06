470e68ac4cc44c664743006e34f56ee0a7c5659d
yunea <code.experience01@gmail.com>
2024-03-06 22:46:25 +0100

    delete Button component (not used) in Profil.js

--- src/frontend/webappService/src/pages/Profil.js
+++ src/frontend/webappService/src/pages/Profil.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import "../css/App.css";
 import UserProfil from "../components/UserProfil.js";
-import Button from "../components/Button.js";
 
 function Profil() {
   const userId = localStorage.getItem("id");
