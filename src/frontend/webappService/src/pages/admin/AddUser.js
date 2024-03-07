52498880c933979fb66d313b5840452c31316875
yunea <code.experience01@gmail.com>
2024-03-07 22:29:27 +0100

    add comments in AddUser.js

--- src/frontend/webappService/src/pages/admin/AddUser.js
+++ src/frontend/webappService/src/pages/admin/AddUser.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
-import AddUserForm from "../../components/AddUserForm";
-import CheckAdminConnection from "../../components/CheckAdminConnection";
+import Navbar from "../../components/Navbar"; // Importing Navbar component
+import AddUserForm from "../../components/AddUserForm"; // Importing AddUserForm component
+import CheckAdminConnection from "../../components/CheckAdminConnection"; // Importing CheckAdminConnection component
 
 function AddUser() {
   return (
     <>
-      <CheckAdminConnection />
-      <Navbar navUser="admin" />
-      <AddUserForm title="Add a User" />
+      <CheckAdminConnection /> {/* Render the CheckAdminConnection component to check jwt exists and if user is admin */}
+      <Navbar navUser="admin" /> {/* Render the Navbar component with the prop navUser='admin' to manage navbar content and color */}
+      <AddUserForm title="Add a User" /> {/* Render the AddUserForm component with the prop title */}
     </>
   );
 }
