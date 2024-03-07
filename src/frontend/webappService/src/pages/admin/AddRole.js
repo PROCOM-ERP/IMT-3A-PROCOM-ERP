cb99b3b4d2be4f4a46118f64906c574c415e1839
yunea <code.experience01@gmail.com>
2024-03-07 22:34:01 +0100

    change file name AddUserForm to UserForm change import path in AddUser.js

--- src/frontend/webappService/src/pages/admin/AddRole.js
+++ src/frontend/webappService/src/pages/admin/AddRole.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import RoleForm from '../../components/RoleForm';
-import Navbar from '../../components/Navbar';
-import CheckAdminConnection from '../../components/CheckAdminConnection';
+import React from 'react'; // Importing React library
+import RoleForm from '../../components/RoleForm'; // Importing RoleForm component
+import Navbar from '../../components/Navbar'; // Importing Navbar component
+import CheckAdminConnection from '../../components/CheckAdminConnection'; // Importing CheckAdminConnection component
 
 function AddRole() {
   return (
     <>
-      <CheckAdminConnection />
-      <Navbar navUser='admin' />
-      <RoleForm />
+      <CheckAdminConnection /> {/* Render the CheckAdminConnection component to check jwt exists and if user is admin */}
+      <Navbar navUser='admin' /> {/* Render the Navbar component with the prop navUser='admin' to manage navbar content and color */}
+      <RoleForm /> {/* Render the RoleForm component */}
     </>
   )
 }
 
-export default AddRole
+export default AddRole; // Exporting AddRole component
--- src/frontend/webappService/src/pages/admin/AddUser.js
+++ src/frontend/webappService/src/pages/admin/AddUser.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Navbar from "../../components/Navbar"; // Importing Navbar component
-import AddUserForm from "../../components/AddUserForm"; // Importing AddUserForm component
 import CheckAdminConnection from "../../components/CheckAdminConnection"; // Importing CheckAdminConnection component
+import Navbar from "../../components/Navbar"; // Importing Navbar component
+import UserForm from "../../components/admin/UserForm"; // Importing AddUserForm component
 
 function AddUser() {
   return (
     <>
       <CheckAdminConnection /> {/* Render the CheckAdminConnection component to check jwt exists and if user is admin */}
       <Navbar navUser="admin" /> {/* Render the Navbar component with the prop navUser='admin' to manage navbar content and color */}
-      <AddUserForm title="Add a User" /> {/* Render the AddUserForm component with the prop title */}
+      <UserForm title="Add a User" /> {/* Render the AddUserForm component with the prop title */}
     </>
   );
 }
