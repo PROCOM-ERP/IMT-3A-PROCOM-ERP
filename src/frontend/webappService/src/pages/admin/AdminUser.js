5c5ced81888ca7c1b6f46546360fc23523271890
yunea <code.experience01@gmail.com>
2024-03-07 22:16:31 +0100

    add useEffect hook to check userId value in url when /user/{userId} page is called in AdminUser.js add comments to AdminUser.js

--- src/frontend/webappService/src/pages/admin/AdminUser.js
+++ src/frontend/webappService/src/pages/admin/AdminUser.js
@@ -1,19 +1,35 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import UserProfilAdmin from '../../components/UserProfilAdmin'
-import { useParams } from 'react-router-dom';
-import CheckAdminConnection from '../../components/CheckAdminConnection';
+import React from 'react'; // Import React library
+import { useParams } from 'react-router-dom'; // Import useParams hook from react-router-dom
+import Navbar from '../../components/Navbar'; // Import Navbar component
+import UserProfilAdmin from '../../components/UserProfilAdmin'; // Import UserProfilAdmin component
+import CheckAdminConnection from '../../components/CheckAdminConnection'; // Import CheckAdminConnection component
 
 function AdminUser() {
-  const props = useParams();
-  const userId = props.userId;
+
+  const { userId } = useParams(); // Get the userId parameter from the URL using useParams hook
+  const navigate = useNavigate(); // Hook to navigate programmatically
+
+  // useEffect hook to check if userId format is correct
+  useEffect(() => {
+    // Regular expression to check if userId starts with 'A' followed by exactly 5 digits
+    const isValidUserId = /^A\d{5}$/.test(userId);
+
+    // Redirect to error 403 page if userId format is not valid
+    if (!isValidUserId) {
+      navigate("/error403");
+    }
+  }, [userId, navigate]); // Dependencies for useEffect: userId and navigate
+
+  // Render the AdminUser component
   return (
     <>
+      {/* Render the CheckAdminConnection component to check jwt exists and if user is admin */}
       <CheckAdminConnection />
+      {/* Render the Navbar component with the prop navUser='admin' to manage navbar content and color */}
       <Navbar navUser='admin' />
+      {/* Render the UserProfilAdmin component with the title "User Profil" and the userId obtained from the URL */}
       <UserProfilAdmin title="User Profil" userId={userId} />
     </>
-  )
+  );
 }
-
-export default AdminUser
+export default AdminUser;
