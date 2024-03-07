7205c24e837d3a00fd9f0e4670edf053d81ca51f
yunea <code.experience01@gmail.com>
2024-03-07 21:56:13 +0100

    add comments to AdminHome.js

--- src/frontend/webappService/src/pages/admin/AdminHome.js
+++ src/frontend/webappService/src/pages/admin/AdminHome.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import CheckAdminConnection from '../../components/CheckAdminConnection'
+import React from 'react'; // Import React library
+import Navbar from '../../components/Navbar'; // Import Navbar component
+import CheckAdminConnection from '../../components/CheckAdminConnection'; // Import CheckAdminConnection component
 
 function AdminHome() {
-
+  // Render the AdminHome component
   return (
     <>
+      {/* Render the CheckAdminConnection component -> check jwt exists and if user is admin*/}
       <CheckAdminConnection />
+      {/* Render the Navbar component with the prop navUser='admin' -> manage navbar content and color */}
       <Navbar navUser='admin' />
     </>
-  )
+  );
 }
-
-export default AdminHome
+export default AdminHome;
