90a54da43a8925a7aa174d7cca832289dc1690da
bops <thibaut.ruzicka@imt-atlantique.net>
2024-03-10 00:29:20 +0100

    Add: security DTO and repositories

---
+++ src/backend/inventoryService/src/main/java/com/example/inventoryservice/dto/RoleActivationResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.inventoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoleActivationResponseDto {
+
+    private String name;
+    private String microservice;
+    private Boolean isEnable;
+
+}
