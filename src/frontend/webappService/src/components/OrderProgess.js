43b0066d5678d2ba8e3ffaf0d204f54c34260bac
yunea <code.experience01@gmail.com>
2024-03-18 17:41:11 +0100

    orderProgress: - add comments - code is optimized displayOrders: - change idOrder props to orderId

--- src/frontend/webappService/src/components/OrderProgess.js
+++ src/frontend/webappService/src/components/OrderProgess.js
@@ -2,18 +2,34 @@ import React, { useState, useEffect } from 'react';
 import Button from './Button.js';
 import { useNavigate } from 'react-router-dom';
 
-function OrderProgess({ data, approver, idOrder }) {
+function OrderProgess({ data, approver, orderId }) {
   const navigate = useNavigate();
   const [status, setStatus] = useState(0);
 
+  // Function to get the ID of the first incomplete progress
   const getFirstIncompleteProgressId = () => {
     const firstIncompleteProgress = data.find(progress => !progress.completed);
     return firstIncompleteProgress ? firstIncompleteProgress.id : 0;
   };
 
+  // Function to generate API URL
+  const generateApiUrl = (orderId) => {
+    return `https://localhost:8041/api/order/v1/orders/${orderId}/progress`;
+  };
+
+  // Function to handle API errors
+  const handleApiErrors = (response) => {
+    if (!response.ok) {
+      if (response.status === 401) { navigate("/error401"); }
+      else if (response.status === 403) { navigate("/error403"); }
+      else { throw new Error(response.status + " " + response.statusText); }
+    }
+  };
+
+  // Function to validate progress status
   const validateProgressStatus = async (statusValue) => {
     console.log("status: ", statusValue);
-    const apiUrl = `https://localhost:8041/api/order/v1/orders/${idOrder}/progress`;
+    const apiUrl = generateApiUrl(orderId);
     // Make the API request
     try {
       const response = await fetch(apiUrl, {
@@ -24,19 +40,15 @@ function OrderProgess({ data, approver, idOrder }) {
         },
         body: JSON.stringify({ idProgressStatus: statusValue })
       });
-      if (!response.ok) {
-        if (response.status === 401) { navigate("/error401"); }
-        else if (response.status === 403) { navigate("/error403"); }
-        else { throw new Error(response.status + " " + response.statusText); }
-      } else {
-        // If the API request is successful, reload the page
-        window.location.reload();
-      }
+      handleApiErrors(response);
+      // If the API request is successful, reload the page
+      window.location.reload();
     } catch (error) {
       console.error("API request error: ", error);
     }
-  }
+  };
 
+  // Effect to validate progress status when status changes
   useEffect(() => {
     if (status !== 0) {
       console.log("STATUS:", status);
@@ -44,28 +56,28 @@ function OrderProgess({ data, approver, idOrder }) {
     }
   }, [status]);
 
+  // Handler for modifying progress
   const handleModifyProgress = () => {
     const progressId = getFirstIncompleteProgressId(); // Get the progress id
     setStatus(progressId); // Set the status state with the progress id
-  }
+  };
 
   return (
     <>
       <div className='title2'>Progress</div>
-      {
-        data.map((progress, index) => (
-          <div className="information" key={index}>
-            <div className="key-container">{progress.name}: </div>
-            <div className="value-container">{progress.completed ? 'Done' : 'Ongoing'}</div>
-          </div>
-        ))
-
-      }
+      {/* Render progress information */}
+      {data.map((progress, index) => (
+        <div className="information" key={index}>
+          <div className="key-container">{progress.name}: </div>
+          <div className="value-container">{progress.completed ? 'Done' : 'Ongoing'}</div>
+        </div>
+      ))}
+      {/* Render validate button for approver */}
       {(approver == localStorage.getItem('id')) && (
         <Button onClick={handleModifyProgress}>Validate</Button>
       )}
     </>
-  )
+  );
 }
 
 export default OrderProgess;
