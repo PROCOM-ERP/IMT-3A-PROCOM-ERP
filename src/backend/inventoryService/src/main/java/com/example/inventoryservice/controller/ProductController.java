3ac72dbd9ec0cbc78c3a21e85a5d789720e7952d
Arthur Maquin <arthur.maquin@imt-atlantique.net>
2024-03-21 15:00:38 +0100

    sec-inv-branch Fix : API hotfixes

--- src/backend/inventoryService/src/main/java/com/example/inventoryservice/controller/ProductController.java
+++ src/backend/inventoryService/src/main/java/com/example/inventoryservice/controller/ProductController.java
@@ -22,9 +22,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
